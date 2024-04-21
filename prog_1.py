c01ca2c792ca84987ee204ce4b00810cbab494b6
Navyachhoker <143236749+Navyachhoker@users.noreply.github.com>
2024-04-22 00:23:12 +0530

    Add files via upload

---
+++ prog_1.py
@@ -0,0 +1,13 @@
+#to show the use of bullt in mathematical function in python
+print(pow(2,4))
+print(abs(-8))
+print(min(3,5,8,1))
+print(max(96,4,56,3))
+# to show the use of the methods from the math module in python
+import math
+x=49
+print(math.pi, math.e)
+print(math.sqrt(x))
+print(math.factorial(6))
+print(math.log(x))
+print(math.floor(x),math.ceil(x))
---
+++ prog_2.py
@@ -0,0 +1,21 @@
+#to print 'Bamboozled' in different ways
+s="Bamboozled"
+# to extract B a
+print(s[0],s[1])
+# to extract e d
+print(s[-2],s[-1])
+print(s[-2:])
+#extract mboozled
+print(s[2:10])
+print(s[2:])
+print(s[-8:])
+# to extract bamboo
+print(s[0:6])
+print(s[:6])
+print(s[-10:4])
+#reverse Bamboozled
+print(s[::-1])
+s=s+'Hype!'
+print(s)
+s = s[:6]+'Monger'+s[-1]
+print(s)
